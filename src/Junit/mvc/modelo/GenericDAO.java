caa52a2f2a557b8c1b983e2ce7d8469db3a01287
Apitz-Eulogio <Apitz-Eulogio@192.168.0.19>
2016-08-03 22:01:36 -0500

    Gerarquia

---
+++ src/Junit/mvc/modelo/GenericDAO.java
@@ -0,0 +1,20 @@
+package junit.mvc.modelo;
+
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface GenericDAO<T, id extends Serializable> {
+
+	List buscarTodos();
+
+	boolean insertar(T objeto);
+
+	boolean salvar(T objeto);
+
+	T buscarPorClave(id id);
+
+	boolean borrar(T objeto);
+
+}
